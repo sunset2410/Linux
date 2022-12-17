31c64dcd8c78437c827ed24320d7a5bfacb916df
chieubkhn <chieubkhn@gmail.com>
2022-12-17 22:21:10 +0700

    convert char to int

---
+++ convert_char_to_int.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+
+
+
+int main()
+{
+    std::string  str = "AFDS.ASD.12092022.13"; // 20 digit
+    char content[21];
+
+    for(int i =0; i<20;i++) {
+         content[i]= static_cast<int>(str.at(i));  
+    }
+
+
+    for(int i =0; i<20; i++) {
+        printf("%c", content[i]);
+    }
+
+    std::string a  = content;
+
+
+    return 0;
+}
