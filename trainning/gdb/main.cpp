ed13b9722d0fea57711a6b87686019d271f7bd19
chieubkhn <chieubkhn@gmail.com>
2023-01-20 02:08:57 +0700

    123

---
+++ trainning/gdb/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+void mfunction() {
+    int i =100;
+    int b =0;
+    i= i/b;
+    i++;
+}
+
+void test() {
+    int a = 10;
+    mfunction();
+    a++;
+}
+
+int main() {
+    int a = 10;
+    int b = 30;
+    a++;
+    b--;
+    test();
+    cout <<"a= " << a << endl;
+    cout << "b=" << b << endl;
+    return 0;
+}
