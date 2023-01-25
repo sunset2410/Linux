13e00e18299bff2b9867a8e698fc791f660371d7
chieubkhn <chieubkhn@gmail.com>
2023-01-25 21:36:29 +0700

    MyVector

--- trainning/Cplussplus/Myvector.cpp
+++ trainning/Cplussplus/Myvector.cpp
@@ -4,12 +4,17 @@ using namespace std;
 class MyVector {
 private:
     int* arr;
-    int size; 
+    int size = 0; 
+    int max_size = 10;
 public:
     MyVector(int s) {
         size = s;
         arr = new int[size];
     }   
+
+    MyVector() {
+        arr = new int[max_size];
+    }
 
     ~MyVector() {
         delete[] arr;
@@ -22,6 +27,20 @@ public:
     int getSize() const {
         return size;
     }
+
+    void push_back(int a) {
+        if( size == max_size) {
+            max_size *=2;
+            int* arr_temp = new int[max_size];
+            for(int i= 0; i< size; i++) {
+                arr_temp[i] = arr[i];
+            }
+            delete[] arr;
+            arr = arr_temp;
+        }
+        arr[size] = a;
+        size++;
+    }
 };
 
 int main() {
@@ -30,8 +49,13 @@ int main() {
         vt[i] = i*2;
     }
 
+    vt.push_back(100);
+    vt.push_back(101);
+    vt.push_back(102);
+
     for(int i =0; i< vt.getSize(); i ++) {
         cout << " " << vt[i] << " ";
     }
+
     return 0;
 }
